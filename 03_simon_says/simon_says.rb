dffb0010e8ea58860ca2c8f0e332ec45e8cfd675
tarasdemyanets <tarasdemyanets1803@gmail.com>
2019-05-03 14:51:10 +0300

    simon says tas changes

--- 03_simon_says/simon_says.rb
+++ 03_simon_says/simon_says.rb
@@ -1,45 +1,35 @@
 def echo(s)
   return s
 end
+
 def shout(s)
   return s.upcase
 end
+
 def repeat(s, repeat=2)
-  s=s+" "
-  s=s*repeat
-  s=s.strip
-  return s
+  s += ' '
+  s *= repeat
+  s = s.strip
+  s
 end
-def start_of_word(s,letters)
-  s=s[0..letters-1]
+
+def start_of_word(word, letters)
+  word[0..letters - 1]
 end
+
 def first_word(s)
-  s=s.split(" ")
-  first_word=s[0]
-  return first_word
+  s = s.split(' ')
+  word = s[0]
+  word
 end
-def titleize(s)
-  articles=["of","the","a", "to", "in", "of", "on","and","over"]
-  array_of_words=s.split(" ")
-  w=array_of_words[0]
-  array_of_words[0]=w.capitalize
-  if array_of_words.length==1
-    s=s.capitalize
-  else
-  for i in 1..array_of_words.length-1
-    includes=false
-    word=array_of_words[i]
-    for j in 0..articles.length
-      if word == articles[j]
-        includes=true
-      end
-    end
-    if includes==true
-    else
-      array_of_words[i]=word.capitalize
-    end
+
+def titleize(expression)
+  articles = %w[of the a to in of on and over]
+  array_of_words = expression.split(' ')
+  word = array_of_words[0]
+  array_of_words[0] = word.capitalize
+  array_of_words.each_with_index do |w, i|
+    array_of_words[i] = w.capitalize unless articles.include?(w)
   end
-  s=array_of_words.join(" ")
-end
-  return s
+  array_of_words.join(' ')
 end
